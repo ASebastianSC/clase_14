b9adc5e83d5e45df0ff3a2e625ba54cc03a30a0b
Sebastian Subizar <mahoushonen@99.200-57-1.bestelclientes.com.mx>
2019-06-06 21:07:14 -0500

    final

--- expressact.js
+++ expressact.js
@@ -1,27 +1,23 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 9000
 
 app.listen(port, function () { 
     console.log(`Example app listening on port ${port}!`)
 })
         
 app.get('/', function(req, res) { 
-    console.log(`Contestando una petición por GET`)
-})
-    
-app.get('/', function(req, res) { 
-    console.log(`Contestando una petición por GET`)
+    res.send(`Contestando una petición por GET`)
 })
     
 app.post('/', function(req, res) { 
-    console.log(`Contestando una petición por POST`)
+    res.send(`Contestando una petición por POST`)
 })
 
 app.put('/', function(req, res) { 
-    console.log(`Contestando una petición por PUT`)
+    res.send(`Contestando una petición por PUT`)
 })
 
 app.delete('/', function(req, res) { 
-    console.log(`Contestando una petición por DELETE`)
+    res.send(`Contestando una petición por DELETE`)
 })
